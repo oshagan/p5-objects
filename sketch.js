2d2dda42d400304ae0a93f153aa3437924177436
Adom Oshagan <97771232+oshagan@users.noreply.github.com>
2023-03-28 20:04:22 +0000

    First part of activity - DONE

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+class Rock{
+  constructor(x, altitude,speed){
+    this.x = x;
+    this.altitude = altitude;
+    this.speed = speed;
+  }
+
+  draw(){
+    ellipse(this.x, height - this.altitude, 10);
+    this.altitude -= this.speed;
+    if(this.altitude <= 4){
+      this.altitude = 4;
+    }
+  }
+}
+
+let myRock;
+let leftRock;
+
 function setup () {
-  // put setup code here
+  createCanvas(300,300);
+  myRock = new Rock(width/2,height,2);
+  leftRock = new Rock(100,height,1.5);
 }
 
 function draw () {
-  // put drawing code here
+  background(200);
+  myRock.draw();
+  leftRock.draw();
 }
