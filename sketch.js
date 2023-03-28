5ee77349041fe843bbab5df02a1ff495160c220b
Adom Oshagan <97771232+oshagan@users.noreply.github.com>
2023-03-28 20:20:52 +0000

    inherited

--- sketch.js
+++ sketch.js
@@ -5,6 +5,14 @@ class Rock{
     this.speed = speed;
   }
 
+  update(){
+    ellipse(this.x, height - this.altitude, 10);
+    this.altitude -= this.speed;
+    if(this.altitude <= 4){
+      this.altitude = 4;
+    }
+  }
+
   draw(){
     ellipse(this.x, height - this.altitude, 10);
     this.altitude -= this.speed;
@@ -14,13 +22,24 @@ class Rock{
   }
 }
 
+class ParaRock extends Rock{
+  constructor(x, altitude,speed){
+    super(x, altitude,speed);
+  }
+  draw(){
+    ellipse(this.x, height - this.altitude - 10, 30);
+    text('override', this.x, height - this.altitude, 10);
+    this.update();
+  }
+}
+
 let myRock;
 let leftRock;
 
 function setup () {
   createCanvas(300,300);
   myRock = new Rock(width/2,height,2);
-  leftRock = new Rock(100,height,1.5);
+  leftRock = new ParaRock(100,height,1.5);
 }
 
 function draw () {
