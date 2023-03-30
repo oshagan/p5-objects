4183793a6268865e381d36ae5ccb282880a8583b
Adom Oshagan <97771232+oshagan@users.noreply.github.com>
2023-03-30 20:20:37 +0000

    Emoji Wars Done with HOPs

--- sketch.js
+++ sketch.js
@@ -1,28 +1,59 @@
-class Rock{
-  constructor(x, altitude,speed){
+class Sprite {
+  constructor(x, y, drawFunction){
     this.x = x;
-    this.altitude = altitude;
+    this.y = y;
+    this.drawFunction = drawFunction;
+  }
+  draw() {
+    this.update();
+    this.drawFunction();
+  }
+  // update: change the state with each frame
+  update() {
+
+  }
+}
+
+class EmojiHead extends Sprite {
+  constructor(x, y, speed, emoji, goingUp){
+    super(x, y, () => text(this.emoji, this.x, this.y));
     this.speed = speed;
+    this.emoji = emoji;
+    this.up = goingUp;
   }
 
-  update(){
-    ellipse(this.x, height - this.altitude, 10);
-    this.altitude -= this.speed;
-    if(this.altitude <= 4){
-      this.altitude = 4;
+  update() {
+    if(this.up){
+      this.y -= this.speed;
     }
-  }
-
-  draw(){
-    ellipse(this.x, height - this.altitude, 10);
-    this.altitude -= this.speed;
-    if(this.altitude <= 4){
-      this.altitude = 4;
+    else{
+      this.y += this.speed;
+    }
+    if(this.y > height){
+      this.up = true;
+    }
+    if(this.y < 0){
+      this.up = false;
     }
   }
 }
 
-class ParaRock extends Rock{
+class Rock extends Sprite {
+  constructor(x, altitude, speed) {
+    super(x, height - altitude, () => ellipse(this.x, this.y, 10));
+    this.speed = speed;
+  }
+
+  update(){
+    this.y += this.speed;
+    if(this.y >= height - 4){
+      this.y = height - 4;
+    }
+  }
+
+}
+
+class ParaRock extends Rock {
   constructor(x, altitude,speed){
     super(x, altitude,speed);
   }
@@ -33,17 +64,21 @@ class ParaRock extends Rock{
   }
 }
 
-let myRock;
-let leftRock;
+let head1;
+let head2;
+let rock;
 
 function setup () {
   createCanvas(300,300);
-  myRock = new Rock(width/2,height,2);
-  leftRock = new ParaRock(100,height,1.5);
+  head1 = new EmojiHead(100, 0, 2, '😀', false);
+  head2 = new EmojiHead(200, height, 1, '😅', true)
+  rock = new Rock(width/2, height, 1);
+  // leftRock = new ParaRock(100,height,1.5);
 }
 
 function draw () {
   background(200);
-  myRock.draw();
-  leftRock.draw();
+  head1.draw();
+  head2.draw();
+  rock.draw();
 }
